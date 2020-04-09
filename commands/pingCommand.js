87bf78c8997747ecd114ad2aa1145969b9cdb3e8
hansputera <47862061+hansputera@users.noreply.github.com>
2020-04-09 12:48:19 +0700

    space

--- commands/pingCommand.js
+++ commands/pingCommand.js
@@ -1,4 +1,4 @@
 module.exports.exec = async (bot, msg, args) => {
 let ping = bot.ws.ping;
-message.channel.send(`:ping_pong: Pong! \`\`${ping}ms\`\``);
+message.channel.send(`:ping_pong: Pong! \n\`\`${ping}ms\`\``);
 });
