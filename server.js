0daedc6b21d04265d2376536ff0fc94b98c4d789
hansputera <47862061+hansputera@users.noreply.github.com>
2020-04-11 13:55:33 +0700

    Sekian:V

--- server.js
+++ server.js
@@ -6,6 +6,27 @@ const session = require('express-session');
 const passport = require('passport');
 const Strategy = require('passport-discord').Strategy;
 
+passport.deserializeUser(function(obj, done) {
+done(null, obj);
+});
+
+passport.serializeUser(function(user, done) {
+done(null, user);
+});
+
+let scopes = ['guilds','email','identify'];
+
+passport.use(new Strategy({
+clientID: process.env.ID,
+clientSecret: process.env.Recipe,
+callbackURL: process.env.Colibek,
+scope: scopes
+}, function(accessToken, refreshToken, profile, done) {
+process.nextTick(function() {
+return done(null, profile);
+});
+}));
+
 app.get('/', (req, res) => {
 res.sendStatus(200);
 });
