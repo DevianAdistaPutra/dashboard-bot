2a7d6c03b3947ac9776960c0458ffd63c93722df
hansputera <47862061+hansputera@users.noreply.github.com>
2020-04-11 14:04:09 +0700

    Update server.js

--- server.js
+++ server.js
@@ -7,6 +7,9 @@ const session = require('express-session');
 const passport = require('passport');
 const Strategy = require('passport-discord').Strategy;
 
+
+app.set('x-powered-by', false);
+app.set('view engine', 'ejs');
 passport.deserializeUser(function(obj, done) {
 done(null, obj);
 });
@@ -38,7 +41,14 @@ saveUninitialized: false
 app.use(passport.session());
 app.use(passport.initialize());
 
+app.get('/api/discord/login', passport.authenticate('discord', { scope: scopes }), function(req,res) {
 
+});
+
+app.get('/api/discord/colbek', passport.authenticate('discord', { failureRedirect: '/login' }), function(req,res) {
+console.log(`[LOGIN] ${req.user.username}#${req.user.discriminator} telah login!`);
+res.redirect('/');
+});
 
 app.get('/', (req, res) => {
 res.sendStatus(200);
