e11298d320b296aeb85971ea92f84586a6deb1c4
hansputera <47862061+hansputera@users.noreply.github.com>
2020-04-11 19:33:15 +0700

    statistics

---
+++ commands/statsCommand.js
@@ -0,0 +1,22 @@
+
+const { MessageEmbed } = require('discord.js');
+const os = require('os');
+
+module.exports.exec = async (bot, msg, args) => {
+    let model = os.cpus()[0].model;
+    let ping = bot.ws.ping;
+
+    const embed = new MessageEmbed()
+    .setTitle(`${bot.user.username} Information's`)
+    .setThumbnail(bot.user.displayAvatarURL())
+    .setDescription(`
+      **• System**
+      \`\`\`css\n
+      - Model: ${model}
+      - Websocket Ping: ${ping}
+      - Presence Status: ${bot.presence.status}
+      \`\`\`
+    `)
+    .setFooter(`• Request: ${msg.author.tag}`);
+message.channel.send({ embed });
+};
