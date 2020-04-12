92e418149166cf1117a0eae5ec4a2900e44d90e5
hansputera <47862061+hansputera@users.noreply.github.com>
2020-04-12 14:23:15 +0700

    quick.db

--- router/autonick.js
+++ router/autonick.js
@@ -1,10 +1,11 @@
 const express = require('express'),
-      router = express.Router();
+      router = express.Router(),
+      db = require('quick.db');
       
 router.get('/settings/autonick', (req,res) => {
 if (!req.user) return res.redirect('/api/discord/login');
 else
-res.render('autonick.ejs', { req: req, res: res });
+res.render('autonick.ejs', { db: db, req: req, res: res });
 });
 
 module.exports = router;
